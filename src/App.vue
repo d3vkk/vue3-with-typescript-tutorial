bebc6e1da49ed880c08d3e28636612f1cdcdec48
d3vkk <donaldkk22@yahoo.com>
2022-05-29 18:15:18 +0300

    feat: Options API with typescript

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-</script>
-
 <template>
-  <div class="app">hello, hyrule</div>
+  <div class="app">
+    <p>{{ name }} is {{ age }} years old</p>
+    <button @click="changeName('Hue')">Change Name</button>
+    <button @click="changeAge('88')">Change Age</button>
+  </div>
 </template>
 
-<style>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      name: "George",
+      // Unions are not allowed in the data property
+      // Hence type assertions are used
+      age: 67 as number | string,
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+    // As age has a double type, the parameter type can be either, or OR both
+    // changeAge(age: number | string) {
+    changeAge(age: string) {
+      this.age = age;
+    },
+  },
+});
+</script>
+
+<style scoped>
 </style>
