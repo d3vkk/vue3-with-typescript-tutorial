84a5dacbcb6a40d8230eaaacf27d54445834162b
d3vkk <donaldkk22@yahoo.com>
2022-06-09 17:29:50 +0300

    feat: Computed properties in typescript

--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -2,7 +2,7 @@
   <div class="jobs-list">
     <p>Ordered by {{ order }}</p>
     <ul>
-      <li v-for="job in jobs" :key="job.id">
+      <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
           <p>{{ job.salary }} USD</p>
@@ -21,7 +21,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { computed, defineComponent, PropType } from "vue";
 import Job from "../types/Job";
 import OrderTerm from "../types/OrderTerm";
 
@@ -36,8 +36,14 @@ export default defineComponent({
       required: true,
     },
   },
-  setup() {
-    return {};
+  setup(props) {
+    // Ordered in ascending order
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1;
+      });
+    });
+    return { orderedJobs };
   },
 });
 </script>
